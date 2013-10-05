3883f17c6db47f24511946e3803530de99aeb5d1
Kit Sunde <kitsunde@gmail.com>
2013-10-06 06:42:52 +0800

    Formatting

--- github/urls.py
+++ github/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import *
 from github.views import AuthorizeView
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)/authorize$',
-        AuthorizeView.as_view(),
-        name='authorize'),
+    url(r'^(?P<pk>\d+)/authorize$', AuthorizeView.as_view(), name='authorize'),
 )
